f9b697c8481110cab80dd830b8cfc79537a1f80b
rewardedtenfold <101484454+rewardedtenfold@users.noreply.github.com>
2024-11-14 21:31:09 +0700

    Add files via upload

---
+++ functiontnc.js
@@ -0,0 +1,7 @@
+//<![CDATA[
+function replacelinkpihakketiga() {
+		divus = divus.replace(new RegExp('REFUNDGOOGLE', 'g'),refundgoogleus);
+		divid = divid.replace(new RegExp('REFUNDGOOGLE', 'g'),refundgoogleid);
+	}
+	replacelinkpihakketiga();
+//]]>
