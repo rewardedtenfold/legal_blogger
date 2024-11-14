75bdf62178e6693c2ab24f1d5afefce5144115d0
rewardedtenfold <101484454+rewardedtenfold@users.noreply.github.com>
2024-11-14 21:02:42 +0700

    Delete functiontnc.js

--- functiontnc.js
+++
@@ -1,5 +0,0 @@
-	function replacelinkpihakketiga() {
-		divus = divus.replace(new RegExp('REFUNDGOOGLE', 'g'),refundgoogleus);
-		divid = divid.replace(new RegExp('REFUNDGOOGLE', 'g'),refundgoogleid);
-	}
-	replacelinkpihakketiga();
